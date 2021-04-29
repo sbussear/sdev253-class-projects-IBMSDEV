bd4e1bc49ed3b1e612cec56e2db8f6a2f48efcbb
sbussear <81540555+sbussear@users.noreply.github.com>
2021-04-29 19:31:17 -0400

    starting M06

---
+++ class_projects/samiproject/bookmark_ap/register_form.php
@@ -0,0 +1,10 @@
+<?php 
+// include function files for this application
+require_once('bookmark_fns.php'); 
+
+do_html_header('User Registration'); 
+
+display_registration_form(); 
+
+do_html_footer();
+?>
